029c13b13cb064ae5bc3f51eebca1e4d418cb83f
zyadkaroune-jpg <zyadkaroune@gmail.com>
2026-06-16 19:39:02 +0100

    Create kc130.js

---
+++ kc130.js
@@ -0,0 +1,7 @@
+var kc130 = geofs.aircrafts.add({
+  model: 'https://raw.githubusercontent.com/<your-username>/<repo>/main/kc130.glb',
+  name: 'KC-130 Hercules',
+  mass: 70300,
+  engines: 4
+});
+Z
